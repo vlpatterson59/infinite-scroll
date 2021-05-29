5585a8042eb30d8c39e14b3a90f9572e11228744
Val Patterson <val@nutznboltz.website>
2021-05-29 15:04:20 -0400

    keeping the code DRY

--- script.js
+++ script.js
@@ -8,18 +8,29 @@ const count = 10;
 const apiKey = `ghExX_J04v3zftksptAj2XpRzyeiIdb5uuHBHTpu9y0`;
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+// Helper Function so Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Create Elements for Links and Photos, Add to DOM
 function displayPhotos() {
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
